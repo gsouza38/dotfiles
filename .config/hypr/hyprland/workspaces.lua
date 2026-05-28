8cebf1b0939c0c65a65de476d989f6860de5aeb5
Gabriel de Souza <167147017+gsouza38@users.noreply.github.com>
2026-05-28 12:02:32 -0300

    Add workspace configuration for Hyprland

---
+++ .config/hypr/hyprland/workspaces.lua
@@ -0,0 +1,14 @@
+hl.config({
+    -- Dwindle layout options
+    dwindle = {
+        preserve_split = true,
+        -- smart_split = true
+    },
+
+    binds = {
+        allow_workspace_cycles = true,
+        movefocus_cycles_fullscreen = false,
+        workspace_back_and_forth = true,
+        workspace_center_on = 1
+    }
+})
