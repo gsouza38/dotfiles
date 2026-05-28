2ae5e0088f7ee3787e3e5882e226c8468073e44b
Gabriel de Souza <167147017+gsouza38@users.noreply.github.com>
2026-05-28 12:00:48 -0300

    Add input configuration for Hyprland

---
+++ .config/hypr/hyprland/input.lua
@@ -0,0 +1,16 @@
+hl.config({
+    input = {
+        kb_layout  = "br",
+        numlock_by_default = true,
+        repeat_rate = 60,
+        repeat_delay = 300,
+        follow_mouse = 1,
+        sensitivity = -0.4,
+        scroll_factor = 2.0,
+        accel_profile = "flat",
+        touchpad = {
+            natural_scroll = true,
+            disable_while_typing = true,
+        },
+    },
+})
